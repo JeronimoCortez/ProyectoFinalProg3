91dcb4d79591c4a20f3ef7477b07796720cf7a59
JeronimoCortez <jeronimofcortez3@gmail.com>
2024-10-17 13:56:02 -0300

    Card company

--- vite-project/src/Components/ui/CardCompany/CardCompany.tsx
+++ vite-project/src/Components/ui/CardCompany/CardCompany.tsx
@@ -12,15 +12,28 @@ const bull = (
 );
 
 export const CardCompany = () => {
-  return;
-  <>
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          Nombre de la Empresa
-        </Typography>
-        <EditButton typeEdit="Company" className={styles.company} />
-      </CardContent>
-    </Card>
-  </>;
+  return (
+    <>
+      <Card
+        sx={{
+          width: 250,
+          height: 150,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#EE964B",
+        }}
+      >
+        <CardContent>
+          <Typography
+            gutterBottom
+            sx={{ color: "#FFFFFF", fontSize: 24, textAlign: "center" }}
+          >
+            Nombre de la Empresa
+          </Typography>
+          <EditButton typeEdit="Company" className={styles.company} />
+        </CardContent>
+      </Card>
+    </>
+  );
 };
