2118c7b419b48cc62c8d47658e24b74ed4b360a8
MauroArz <mauroeze365@gmail.com>
2024-10-21 12:08:35 -0300

    Creando Home seccion Sucursales

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,3 +1,4 @@
+import { Home } from "./Components/screens/Home/Home";
 import { CardBranch } from "./Components/ui/CardBranch/CardBranch";
 import { CardCompany } from "./Components/ui/CardCompany/CardCompany";
 
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       <CardCompany />
-      <CardBranch />
+      <Home />
     </>
   );
 }
--- vite-project/src/Components/screens/Home/Home.tsx
+++ vite-project/src/Components/screens/Home/Home.tsx
@@ -1,3 +1,56 @@
+import { Box, Button } from "@mui/material";
+import { CardBranch } from "../../ui/CardBranch/CardBranch";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Box className="branchInfoContainer" sx={{ backgroundColor: "#0B2545" }}>
+        <Box
+          sx={{
+            color: "#FFFDFD",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "2rem",
+            fontFamily: "Prompt, sans-serif",
+          }}
+        >
+          <h1>SUCURSALES: NOMBRE DE EMPRESA</h1>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            sx={{
+              backgroundColor: "#EE964B",
+              color: "black",
+              width: "120px",
+              height: "55px",
+              margin: "1rem",
+              fontSize: "1.2rem",
+              fontFamily: "Prompt, sans-serif",
+            }}
+          >
+            Agregar
+          </Button>
+        </Box>
+        <Box
+          className="cardBranchesContainer"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <CardBranch />
+          <CardBranch />
+          <CardBranch />
+          <CardBranch />
+        </Box>
+      </Box>
+    </>
+  );
 };
