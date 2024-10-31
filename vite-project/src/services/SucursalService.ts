061aac3e3bc15b98136103ea1e561620683b4b13
JeronimoCortez <jeronimofcortez3@gmail.com>
2024-10-31 11:47:45 -0300

    get sucursal by empresa

--- vite-project/src/services/SucursalService.ts
+++ vite-project/src/services/SucursalService.ts
@@ -4,19 +4,10 @@ import { IUpdateSucursal } from "../types/dtos/sucursal/IUpdateSucursal";
 import { BackendClient } from "./BackendClient";
 
 export class SucursalService extends BackendClient<ISucursal | ICreateSucursal | IUpdateSucursal>{
-  async getSucursalByEmpresaId(idEmpresa: number): Promise<ISucursal[] | null> {
+  async getSucursalByEmpresaId(idEmpresa: number): Promise<ISucursal[]> {
     const response = await fetch(`${this.baseUrl}/porEmpresa/${idEmpresa}`);
     const data = await response.json();
-    const sucursales: ISucursal[] = [];
-    data.map((el: ISucursal) => {
-      if(el.empresa.id === idEmpresa){
-        sucursales.push(el);
-      }
-    })
-    if(sucursales.length > 0){
-      return sucursales;
-    } else {
-      return null
-    }
+    return data;
+  
   }
 }
